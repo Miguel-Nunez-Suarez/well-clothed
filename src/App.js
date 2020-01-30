6d70a5c7dbb41c60e3c2df3b9267cb59a098e4f4
Miguel-Nunez-Suarez <miguel.nuarez@gmail.com>
2020-01-30 19:23:40 +0000

    Basic navigation using  react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/hompage.component";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page!</h1>
+  </div>
+);
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
